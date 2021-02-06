295b5525a5d144b743044c915130dbe75e97f8e4
zzarbttoo <qodbwls064@gmail.com>
2021-02-07 01:02:10 +0900

    urlChecker with go routines and channel

--- main.go
+++ main.go
@@ -1,32 +1,65 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
+
+var errRequestFaild = errors.New("Request failed")
+
+type requestResult struct {
+	url    string
+	status string
+}
 
 func main() {
 
-	//어떤 type 의 information 인지 적어줘야한다
-	channel := make(chan string)
-	people := [5]string{"nico", "flynn", "dal", "japangy", "larry"}
+	results := make(map[string]string)
+	channel := make(chan requestResult)
 
-	//5 routines -> 5 messages
-	for _, person := range people {
-		go isSexy(person, channel)
+	urls := []string{
+
+		"https://nomadcoders.co/",
+		"https://www.naver.com/",
+		"https://www.google.com/",
+		"https://www.youtube.com/",
+		"https://github.com/",
+		"https://www.amazon.com/",
 	}
 
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-channel)
+	for _, url := range urls {
+
+		go hitURL(url, channel)
+	}
+
+	//receiving message
+
+	for i := 0; i < len(urls); i++ {
+		result := <-channel
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-//return 값이 의마가 없다 (go routines 는 어떤 값을 저장할 수 없게 한다)
-// chan channel type
-func isSexy(person string, channel chan string) {
+//write direction
+//can only write
+func hitURL(url string, channel chan<- requestResult) {
 
-	time.Sleep(time.Second * 10)
-	//channel 에 true 를 보내라
-	channel <- person + "is sexy"
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+
+		status = "FAILED"
+
+	}
+
+	channel <- requestResult{url: url, status: status}
+
+	// can send to the channel c<-result{}
+	// but can also recieve from channel
 
 }
