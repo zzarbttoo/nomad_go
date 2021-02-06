39c6194ae034fee5fcfc4cf5a416f3f834a0ec0e
zzarbttoo <qodbwls064@gmail.com>
2021-02-07 00:40:13 +0900

    Add go routines and Channel

--- main.go
+++ main.go
@@ -1,61 +1,32 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-
-	"net/http"
+	"time"
 )
-
-var errRequestFaild = errors.New("Request failed")
 
 func main() {
 
-	var results = make(map[string]string)
+	//어떤 type 의 information 인지 적어줘야한다
+	channel := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japangy", "larry"}
 
-	urls := []string{
-
-		"https://nomadcoders.co/",
-		"https://www.naver.com/",
-		"https://www.google.com/",
-		"https://www.youtube.com/",
-		"https://github.com/",
-		"https://www.amazon.com/",
+	//5 routines -> 5 messages
+	for _, person := range people {
+		go isSexy(person, channel)
 	}
 
-	results["hello"] = "Hello"
-
-	for _, url := range urls {
-
-		result := "OK"
-		err := hitURL(url)
-
-		if err != nil {
-
-			result = "FAILED"
-		}
-
-		results[url] = result
-
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-channel)
 	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
-
 }
 
-func hitURL(url string) error {
+//return 값이 의마가 없다 (go routines 는 어떤 값을 저장할 수 없게 한다)
+// chan channel type
+func isSexy(person string, channel chan string) {
 
-	fmt.Println("Checking :", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFaild
-
-	}
-
-	return nil
+	time.Sleep(time.Second * 10)
+	//channel 에 true 를 보내라
+	channel <- person + "is sexy"
 
 }
